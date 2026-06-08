69823c0bcc697090b2905f42007fccf23f9a2294
Zhang Jiarui <3911926846@qq.com>
2026-06-08 17:42:37 +0800

    feat: 添加单词发音功能，支持英音和美音

--- components/WordCard/WordCard.js
+++ components/WordCard/WordCard.js
@@ -1,4 +1,6 @@
 // WordCard 组件
+const audioPlayer = require('../../utils/audioPlayer.js');
+
 Component({
   /**
    * 组件的属性列表
@@ -15,7 +17,8 @@ Component({
    * 组件的初始数据
    */
   data: {
-    collocationList: []
+    collocationList: [],
+    audioError: ''
   },
 
   /**
@@ -23,6 +26,9 @@ Component({
    */
   methods: {
     onWordChange(word) {
+      // 清除错误提示
+      this.setData({ audioError: '' });
+      
       if (word && word.collocation) {
         // 将分号分隔的搭配字符串转换为数组
         const collocations = word.collocation.split(';').map(item => item.trim()).filter(item => item);
@@ -34,6 +40,56 @@ Component({
           collocationList: []
         });
       }
+    },
+
+    /**
+     * 播放英音
+     */
+    playUK() {
+      const word = this.properties.word.word || this.properties.word.english;
+      if (!word) return;
+      
+      this.setData({ audioError: '' });
+      
+      audioPlayer.playUK(
+        word,
+        (err) => {
+          this.setData({ audioError: err });
+          // 3秒后自动清除错误提示
+          setTimeout(() => {
+            this.setData({ audioError: '' });
+          }, 3000);
+        },
+        () => {
+          // 播放成功，清除错误提示
+          this.setData({ audioError: '' });
+        }
+      );
+    },
+
+    /**
+     * 播放美音
+     */
+    playUS() {
+      const word = this.properties.word.word || this.properties.word.english;
+      if (!word) return;
+      
+      this.setData({ audioError: '' });
+      
+      audioPlayer.playUS(
+        word,
+        (err) => {
+          this.setData({ audioError: err });
+          // 3秒后自动清除错误提示
+          setTimeout(() => {
+            this.setData({ audioError: '' });
+          }, 3000);
+        },
+        () => {
+          // 播放成功，清除错误提示
+          this.setData({ audioError: '' });
+        }
+      );
     }
   },
 
@@ -42,5 +98,13 @@ Component({
    */
   attached() {
     this.onWordChange(this.properties.word);
+    audioPlayer.init();
+  },
+
+  /**
+   * 组件卸载时
+   */
+  detached() {
+    audioPlayer.stop();
   }
 });
